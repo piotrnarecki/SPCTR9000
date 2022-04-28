71e9193c61b919f0518356e628081375e7ca12ed
piotrnarecki <nareckipiotraleksander@gmail.com>
2022-04-28 16:04:39 +0200

    added basics pages

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-from flask import Flask
-
+from flask import Flask, render_template, abort
+from model import db
 
 app = Flask(__name__)
+
 
 # komentarz dla zaznaczenia ze to branch Piotra
 
 
 @app.route("/")
 def my_function():
-    return "Hello World"
+    return render_template("welcome.html",
+                           cards=db
+                           )
 
 
-
-
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html",
+                               card=card,
+                               index=index,
+                               max_index=len(db)-1)
+    except IndexError:
+        abort(404)
---
+++ spctr_controller.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, abort
+from model import db
+
+
+# WPISZ PONISZE KOMENTY W TERMINAL NA DOLE ABY URUCHOMIC
+# export FLASK_APP=spctr9000_controller.py
+# export FLASK_ENV=developer
+# export FLASK_DEBUG=1
+# flask run
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return render_template("spctr_welcome.html")
+
+@app.route("/load_data")
+def load_data():
+    return render_template("spctr_load_data.html")
+
+
+@app.route("/analyse_data")
+def analyse_data():
+    return render_template("spctr_results.html")
