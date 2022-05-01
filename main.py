c39fe0e919dd551e8a63cac2f19391977c605321
piotrnarecki <nareckipiotraleksander@gmail.com>
2022-05-01 11:01:08 +0200

    added html basic template but it is not functional yet

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-from flask import Flask, render_template, abort
-from model import db
+# This is a sample Python script.
 
-app = Flask(__name__)
-
-
-# komentarz dla zaznaczenia ze to branch Piotra
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-@app.route("/")
-def my_function():
-    return render_template("welcome.html",
-                           cards=db
-                           )
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-@app.route("/card/<int:index>")
-def card_view(index):
-    try:
-        card = db[index]
-        return render_template("card.html",
-                               card=card,
-                               index=index,
-                               max_index=len(db)-1)
-    except IndexError:
-        abort(404)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
